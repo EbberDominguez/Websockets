bab28ca8ea719e4c60910eec92288fee9d561dde
Ebber <ebberdominguez@gmail.com>
2021-06-01 20:30:47 -0600

    Creacion de la area de frontend y configuracion de la conexion de sockets

--- Server/main.js
+++ Server/main.js
@@ -10,6 +10,9 @@ app.get('/',function(req,res){
     res.status(200).send('Hola mundo');
 });
 
+io.on('connection',function(socket){
+    console.log('Se han conectado al socket');
+});
 
 server.listen(3002,function(){
     console.log(`El servidor esta corriendo en http://localhost:3002`);
